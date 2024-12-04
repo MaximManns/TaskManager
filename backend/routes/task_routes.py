434ed8bb0d4def0615b25ef10e457ee06d235794
Maxim <gggguuuucci11@gmail.com>
2024-12-04 17:46:30 +0100

    Add HTTP Status code

--- backend/routes/task_routes.py
+++ backend/routes/task_routes.py
@@ -4,6 +4,7 @@ from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 from backend.dependencies import get_db
 from backend.schemas import Task
+from http import HTTPStatus
 
 router = APIRouter()
 
@@ -16,9 +17,9 @@ def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)) ->
 
 @router.post("/tasks/", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)) -> Task:
-    db_user = db.query(db_models.User).filter(db_models.User.id == task.owner_id).first()
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
+    owner = db.query(db_models.User).filter(db_models.User.id == task.owner_id).first()
+    if not owner:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
     new_task = db_models.Task(
         title=task.title,
         description=task.description,
