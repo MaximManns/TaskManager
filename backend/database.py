ed47f9df30e6c0bac64030e1dfb13a7ac623021f
Maxim <gggguuuucci11@gmail.com>
2024-12-06 16:49:40 +0100

    Add try and catch for engine creation

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import logging
 import os
 
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 load_dotenv()
 
 DB_USERNAME = os.getenv("DB_USERNAME")
@@ -11,10 +13,14 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DEV_DB_SERVER_URL = os.getenv("DEV_DB_SERVER_URL")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DEV_DB_SERVER_URL}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+try:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+    Base = declarative_base()
+
+except Exception as e:
+    logging.exception(f"Error while connecting to database: {e}")
