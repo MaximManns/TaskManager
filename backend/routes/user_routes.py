3920824048c49a31c3756859a31fe8c4dbd4bb8a
Maxim <gggguuuucci11@gmail.com>
2024-12-07 13:43:46 +0100

    Add user deletion

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,9 +1,10 @@
 from backend import db_models
 from backend import schemas
-from fastapi import Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter, Response
 from sqlalchemy.orm import Session
 from backend.dependencies import get_db
 from backend.schemas import User
+from http import HTTPStatus
 import hashlib
 
 router = APIRouter()
@@ -13,7 +14,7 @@ router = APIRouter()
 def get_spefific_user(user_id: int, db: Session = Depends(get_db)) -> User:
     specific_user = db.query(db_models.User).filter(db_models.User.id == user_id).first()
     if not specific_user:
-        raise HTTPException(status_code=404, detail="No User with this id registered")
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="No User with this id registered")
     return specific_user
 
 
@@ -21,7 +22,7 @@ def get_spefific_user(user_id: int, db: Session = Depends(get_db)) -> User:
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)) -> User:
     db_user = db.query(db_models.User).filter(db_models.User.email == user.email).first()
     if db_user:
-        raise HTTPException(status_code=400, detail="User with this name already registered")
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="User with this name already registered")
     hashed_password = hashlib.new('sha256')
     hashed_password.update(str.encode(user.password))
     hashed_password.hexdigest()
@@ -36,3 +37,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)) -> User
 def get_list_of_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)) -> list:
     users = db.query(db_models.User).offset(skip).limit(limit).all()
     return users
+
+
+@router.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+    db.delete(user)
+    db.commit()
+    return Response(status_code=HTTPStatus.NO_CONTENT)
