a0b3dab92a4110288e496e06cdbcbd0f42a6e6f6
Maxim <gggguuuucci11@gmail.com>
2024-12-08 16:33:13 +0100

    Add CORS middleware to specify request origins

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,25 @@
-from backend import db_models
 from fastapi import FastAPI
-from backend.database import engine
+from backend import db_models
 from datetime import datetime
+from backend.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.user_routes import router as user_router
 from backend.routes.task_routes import router as task_router
 
 db_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
