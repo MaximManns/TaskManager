1f405e70f140e64ef37a70a54cee665b7dd80a6e
Maxim <gggguuuucci11@gmail.com>
2024-12-09 09:35:55 +0100

    Add docstrings

--- frontend/app.py
+++ frontend/app.py
@@ -7,21 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render the index page."""
     return render_template('index.html')
 
 
 @app.route('/tasks')
 def show_tasks():
+    """Render the tasks page with a list of tasks retrieved from the API."""
     return request_helper.get_request(resource="tasks", template="tasks.html")
 
 
 @app.route('/users')
 def show_users():
+    """Render the users page with a list of users retrieved from the API."""
     return request_helper.get_request(resource="users", template="users.html")
 
 
 @app.route('/create-task', methods=['POST'])
 def create_task():
+    """Handle the creation of a new task and render the updated tasks page."""
     title = request.form['title']
     description = request.form['description']
     owner_id = request.form['owner_id']
@@ -35,6 +39,7 @@ def create_task():
 
 @app.route('/create-user', methods=['POST'])
 def create_user():
+    """Handle the creation of a new user and render the updated users page."""
     email = request.form['email']
     name = request.form['name']
     password = request.form['password']
@@ -48,11 +53,13 @@ def create_user():
 
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
+    """Handle the deletion of a task and render the updated tasks page."""
     return request_helper.delete_request(template="tasks.html", resource="tasks", parameter=task_id)
 
 
 @app.route('/tasks/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    """Handle the deletion of a user and render the updated users page."""
     return request_helper.delete_request(template="users.html", resource="users", parameter=user_id)
 
 
